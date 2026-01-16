625cbde4c1ce369e06bfa1b79f7a1298bd1e8eec
NXRts <dangouu123@gmail.com>
2026-01-16 19:37:55 +0700

    create footer

--- apps/dashboard/src/components/layout/DashboardLayout.tsx
+++ apps/dashboard/src/components/layout/DashboardLayout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Sidebar } from './Sidebar';
 import { Header } from './Header';
+import { Footer } from './Footer';
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
@@ -12,8 +13,11 @@ export function DashboardLayout({ children }: DashboardLayoutProps) {
       <Sidebar />
       <main className="flex-1 flex flex-col min-w-0 overflow-hidden relative">
         <Header />
-        <div className="flex-1 overflow-y-auto p-8 space-y-8">
-            {children}
+        <div className="flex-1 overflow-y-auto no-scrollbar flex flex-col">
+            <div className="flex-1 p-8 space-y-8">
+                {children}
+            </div>
+            <Footer />
         </div>
       </main>
     </div>
